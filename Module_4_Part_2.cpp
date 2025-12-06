e38df77fe7d15c569818d1d82cbd3a13b1bdd108
Baron Kwong <baronkwonghm@gmail.com>
2025-12-05 23:01:51 -0800

    Update Module_4_Part_2.cpp

--- Module_4_Part_2.cpp
+++ Module_4_Part_2.cpp
@@ -7,22 +7,39 @@ int main() {
     cout << "Enter three numbers: ";
     cin >> n1 >> n2 >> n3;
 
-    if (n1 >= n2) {
-        if (n1 >= n3) {
+    if (n1 > n2) {
+        if (n1 > n3) {
             cout << "Largest number: " << n1;
         }
         else {
             cout << "Largest number: " << n3;
         }
     }
-    else {
-        if (n2 >= n3) {
+    else if (n2 > n3) {
             cout << "Largest number: " << n2;
         }
         else {
             cout << "Largest number: " << n3;
         }
     }
+    else if (n1 == n2) {
+        if (n1 > n3) {
+            cout << "Largest numbers: " << n1 << " and " << n2;
+        }
+    }
+    else if (n1 == n3) {
+        if (n1 > n2) {
+            cout << "Largest numbers: " << n1 << " and " << n3;
+        }
+    }
+    else if (n2 == n3) {
+        if (n2 > n1) {
+            cout << "Largest numbers: " << n2 << " and " << n3;
+    }
+    else {
+        cout << "All numbers are the same.";
+    }
+     
+return 0;
 
-    return 0;
 }
