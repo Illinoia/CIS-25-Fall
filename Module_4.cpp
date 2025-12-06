bc636f0ea51f67e813af2fdea90497acc148a3ee
Baron Kwong <baronkwonghm@gmail.com>
2025-12-05 22:28:54 -0800

    Add files via upload

---
+++ Module_4.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std;
+int main() {
+	int choice;
+	cout << "1. Add Item" << endl << "2. View Items" << endl << "3. Exit" << endl;
+	cout << "Choose an option: ";
+	cin >> choice;
+
+	if (choice == 1) {
+		cout << "Item added!" << endl;
+	}
+	else if (choice == 2) {
+		cout << "Displaying items..." << endl;
+	}
+	else if (choice == 3) {
+		cout << "Exiting..." << endl;
+	}
+	else cout << "Invalid option." << endl;
+	return 0;
+}
